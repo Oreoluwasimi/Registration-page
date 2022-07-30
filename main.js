f4e30782e3b6f04c494e01668bed3deb0fb1ebf0
Oreoluwasimi <oreoluwasimi02@gmail.com>
2022-07-30 07:22:10 +0100

    the registration page was made responsive with a media query

---
+++ main.js
@@ -0,0 +1,13 @@
+const telNumber= document.getElementById("number")
+
+function showNetworkProvider() {
+    if(telNumber.startsWith("07025" || "07026" || "0703" || "0704" || "0706" || "0803" || "0806" || "0810" || "0813" || "0814" || "0816"|| "0903" || "0906" || "+2347025" || "+2347026" || "+234703" || "+234704" || "+234706" || "+234803" || "+234806" || "+234810" || "+234813" || "+234814" || "+234816"|| "+234903" || "+234903")) {
+        telNumber::after.style.
+    }else if(telNumber.startsWith(08102 || +2348102)) {
+        telNumber:after.style.
+    }else if(telNumber.startsWith(08102 || +2348102)) {
+        telNumber:after.style.
+    }
+}
+
+telNumber.addEventListener("input", showNetworkProvider)
